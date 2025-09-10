e70342c9f49ada2b686560f4027e70678e3f463a
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-09-11 00:57:39 +0600

    aaded firebase init

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes/router.jsx";
 import "aos/dist/aos.css";
 import Aos from "aos";
+import AuthProvider from "./context/AuthContext/AuthProvider.jsx";
 
 Aos.init();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="font-urbanist max-w-7xl mx-auto">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>
 );
